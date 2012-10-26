7cce7eb0d88bbe7cb717776fadb2fc7896f6be40
André Gaul <gaul@web-yard.de>
2012-10-26 16:20:29 +0200

    add basic package structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from distutils.core import setup
+
+# shamelessly copied from VoroPy
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup( name = 'krypy',
+       packages = ['krypy', 'tests'],
+       version = '0.1',
+       description = 'Krylov subspace methods for linear algebraic systems',
+       long_description = read('README.md'),
+       author = 'André Gaul',
+       author_email = 'gaul@web-yard.de',
+       url = 'https://github.com/andrenarchy/krypy',
+       classifiers = [
+           'Development Status :: 3 - Alpha',
+           'Intended Audience :: Science/Research',
+           'License :: OSI Approved :: MIT License',
+           'Operating System :: OS Independent',
+           'Programming Language :: Python',
+           'Programming Language :: Python :: 3',
+           'Topic :: Scientific/Engineering :: Mathematics'
+           ],
+       )
