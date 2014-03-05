fe71022017a22cbad03d3941da370592a4abe10f
André Gaul <gaul@web-yard.de>
2014-03-05 17:01:11 +0100

    [recycling] add base classes

--- krypy/recycling.py
+++ krypy/recycling.py
@@ -9,6 +9,51 @@ class _RecyclingStrategy(object):
         pass
 
 
+class _RitzCandidatesGenerator(object):
+    '''Abstract base class for candidate set generation.'''
+    def generate(self, ritz):
+        for i in range(self.max_vectors):
+            yield set(range(i))
+        pass
+
+
+class _DeflationVectorFactory(object):
+    '''Abstract base class for selectors.'''
+    def get(self, solver, P):
+        '''Get deflation vectors.
+
+        :returns: numpy.array of shape ``(N,k)``
+        '''
+        pass
+
+
+class RitzFactory(_DeflationVectorFactory):
+    '''Select Ritz vectors.'''
+    def __init__(self, candidate_generator, candidate_evaluator):
+        self.candidate_generator = candidate_generator
+        self.candidate_evaluator = candidate_evaluator
+
+    def get(self, solver, P):
+        ritz = Ritz(solver.V, solver.H)
+        return ritz.V[:, self._get_best_candidate(solver, P, ritz)]
+
+    def _get_best_candidate(self, solver, P, ritz):
+        '''Return candidate set with least goal functional.'''
+        evaluations = {}
+
+        # evaluate candidate
+        for candidate in self.candidate_generator.generate(solver, P, ritz):
+            try:
+                evaluations[candidate] = \
+                    self.candidates_evaluator.evaluate(solver, P, ritz,
+                                                       candidate)
+            except utils.AssumptionError:
+                # no evaluation possible -> move on
+                pass
+
+        return min(evaluations, key=evaluations.get)
+
+
 def _get_ritz(solver, P, mode='ritz', hermitian=False):
     n = solver.iter
     V = solver.V[:, :n+1]
