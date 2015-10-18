18cffb3481bc627d0a806c23ec225471c79de018
André Gaul <andre@gaul.io>
2015-10-18 17:48:10 +0200

    remove mocks

--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,6 @@
 
 import sys, os
 
-# According to hint from
-# <http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/>.
-import mock
-MOCK_MODULES = ['scipy',
-                'scipy.linalg', 'scipy.linalg.blas',
-                'scipy.sparse', 'scipy.sparse.sputils']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
