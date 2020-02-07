efd6bbeb312fa9d887c4c034d571f9e8b0442809
Nico Schlömer <nico.schloemer@gmail.com>
2020-02-07 20:14:12 +0100

    convenience wrappers: relax test tolerance a bit for Python 3.8

--- krypy/tests/test_convenience_wrappers.py
+++ krypy/tests/test_convenience_wrappers.py
@@ -1,9 +1,9 @@
 import krypy
 import numpy
-import pytest
 
 
 def test_cg_matrix():
+    tol = 1.0e-11
     method = krypy.cg
     ref = [1004.1873775173957, 1000.0003174916551, 999.9999999997555]
 
@@ -13,12 +13,13 @@ def test_cg_matrix():
     out = method(A, b)
     print(out)
 
-    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < 1.0e-12 * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < 1.0e-12 * ref[1]
-    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < 1.0e-12 * ref[2]
+    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < tol * ref[0]
+    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < tol * ref[1]
+    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < tol * ref[2]
 
 
 def test_minres_matrix():
+    tol = 1.0e-11
     method = krypy.minres
     ref = [1004.187372488912, 1000.0003124632159, 999.9999949713145]
 
@@ -28,12 +29,13 @@ def test_minres_matrix():
     out = method(A, b)
     print(out)
 
-    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < 1.0e-12 * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < 1.0e-12 * ref[1]
-    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < 1.0e-12 * ref[2]
+    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < tol * ref[0]
+    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < tol * ref[1]
+    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < tol * ref[2]
 
 
 def test_gmres_matrix():
+    tol = 1.0e-11
     method = krypy.gmres
     ref = [1004.1873724888546, 1000.0003124630923, 999.999994971191]
 
@@ -43,12 +45,13 @@ def test_gmres_matrix():
     out = method(A, b)
     print(out)
 
-    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < 1.0e-12 * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < 1.0e-12 * ref[1]
-    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < 1.0e-12 * ref[2]
+    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < tol * ref[0]
+    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < tol * ref[1]
+    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < tol * ref[2]
 
 
 def test_deflate_cg():
+    tol = 1.0e-11
     method = krypy.cg
     ref = [1004.1873775173271, 1000.0003174918709, 1000.0]
 
@@ -61,12 +64,13 @@ def test_deflate_cg():
     U[0] = 1.0
     out = method(A, b, U=U)
 
-    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < 1.0e-12 * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < 1.0e-12 * ref[1]
-    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < 1.0e-12 * ref[2]
+    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < tol * ref[0]
+    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < tol * ref[1]
+    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < tol * ref[2]
 
 
 def test_deflate_minres():
+    tol = 1.0e-11
     method = krypy.minres
     ref = [1004.1873774950692, 1000.0003174918709, 1000.0]
 
@@ -79,12 +83,13 @@ def test_deflate_minres():
     U[0] = 1.0
     out = method(A, b, U=U)
 
-    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < 1.0e-12 * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < 1.0e-12 * ref[1]
-    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < 1.0e-12 * ref[2]
+    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < tol * ref[0]
+    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < tol * ref[1]
+    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < tol * ref[2]
 
 
 def test_deflate_gmres():
+    tol = 1.0e-11
     method = krypy.gmres
     ref = [1004.1873774950692, 1000.0003174918709, 1000.0]
 
@@ -97,12 +102,13 @@ def test_deflate_gmres():
     U[0] = 1.0
     out = method(A, b, U=U)
 
-    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < 1.0e-12 * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < 1.0e-12 * ref[1]
-    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < 1.0e-12 * ref[2]
+    assert abs(numpy.sum(numpy.abs(out.xk)) - ref[0]) < tol * ref[0]
+    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref[1]) < tol * ref[1]
+    assert abs(numpy.max(numpy.abs(out.xk)) - ref[2]) < tol * ref[2]
 
 
 def test_custom_inner_product():
+    tol = 1.0e-11
     n = 100
     A = numpy.diag([1.0e-3] + list(range(2, n + 1)))
     b = numpy.ones(n)
@@ -113,8 +119,8 @@ def test_custom_inner_product():
     out = krypy.cg(A, b, inner_product=inner)
 
     ref = 1004.1873775173957
-    assert abs(numpy.sum(numpy.abs(out.xk)) - ref) < 1.0e-12 * ref
+    assert abs(numpy.sum(numpy.abs(out.xk)) - ref) < tol * ref
     ref = 1000.0003174916551
-    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref) < 1.0e-12 * ref
+    assert abs(numpy.sqrt(numpy.dot(out.xk, out.xk)) - ref) < tol * ref
     ref = 999.9999999997555
-    assert abs(numpy.max(numpy.abs(out.xk)) - ref) < 1.0e-12 * ref
+    assert abs(numpy.max(numpy.abs(out.xk)) - ref) < tol * ref
