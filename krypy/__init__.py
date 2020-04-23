d9eca422f545b6e8c272322edbfeed15970bce08
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-23 17:41:09 +0200

    introduce __about__

--- krypy/__init__.py
+++ krypy/__init__.py
@@ -1,5 +1,7 @@
 from . import linsys, deflation, recycling, utils
 from ._convenience import cg, minres, gmres
+from .__about__ import __version__
 
-__all__ = ['linsys', 'deflation', 'recycling', 'utils', 'cg', 'minres', 'gmres']
-__version__ = '2.1.7'
+__all__ = [
+    'linsys', 'deflation', 'recycling', 'utils', 'cg', 'minres', 'gmres', '__version__'
+]
