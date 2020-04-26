26263c8ffeaef052b394b97b154e78dfd825a11e
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-26 23:21:10 +0200

    shorten convenience wrapper tests

--- test/test_convenience_wrappers.py
+++ test/test_convenience_wrappers.py
@@ -1,13 +1,16 @@
 import numpy
+import pytest
 
 import krypy
 
 
-def test_cg_matrix():
+@pytest.mark.parametrize("method, ref", [
+    (krypy.cg, [1004.1873775173957, 1000.0003174916551, 999.9999999997555]),
+    (krypy.gmres, [1004.1873724888546, 1000.0003124630923, 999.999994971191]),
+    (krypy.minres, [1004.187372488912, 1000.0003124632159, 999.9999949713145])
+])
+def test_cg_matrix(method, ref):
     tol = 1.0e-11
-    method = krypy.cg
-    ref = [1004.1873775173957, 1000.0003174916551, 999.9999999997555]
-
     A = numpy.diag([1.0e-3] + list(range(2, 101)))
 
     # Make sure the shapes are alright
@@ -25,79 +28,13 @@ def test_cg_matrix():
     assert abs(numpy.max(numpy.abs(sol)) - ref[2]) < tol * ref[2]
 
 
-def test_minres_matrix():
+@pytest.mark.parametrize("method, ref", [
+    (krypy.cg, [1004.1873775173271, 1000.0003174918709, 1000.0]),
+    (krypy.minres, [1004.1873774950692, 1000.0003174918709, 1000.0]),
+    (krypy.gmres, [1004.1873774950692, 1000.0003174918709, 1000.0])
+])
+def test_deflate_cg(method, ref):
     tol = 1.0e-11
-    method = krypy.minres
-    ref = [1004.187372488912, 1000.0003124632159, 999.9999949713145]
-
-    A = numpy.diag([1.0e-3] + list(range(2, 101)))
-    b = numpy.ones(100)
-
-    sol, _ = method(A, b)
-
-    assert abs(numpy.sum(numpy.abs(sol)) - ref[0]) < tol * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(sol, sol)) - ref[1]) < tol * ref[1]
-    assert abs(numpy.max(numpy.abs(sol)) - ref[2]) < tol * ref[2]
-
-
-def test_gmres_matrix():
-    tol = 1.0e-11
-    method = krypy.gmres
-    ref = [1004.1873724888546, 1000.0003124630923, 999.999994971191]
-
-    A = numpy.diag([1.0e-3] + list(range(2, 101)))
-    b = numpy.ones(100)
-
-    sol, _ = method(A, b)
-
-    assert abs(numpy.sum(numpy.abs(sol)) - ref[0]) < tol * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(sol, sol)) - ref[1]) < tol * ref[1]
-    assert abs(numpy.max(numpy.abs(sol)) - ref[2]) < tol * ref[2]
-
-
-def test_deflate_cg():
-    tol = 1.0e-11
-    method = krypy.cg
-    ref = [1004.1873775173271, 1000.0003174918709, 1000.0]
-
-    n = 100
-    A = numpy.diag([1.0e-3] + list(range(2, n + 1)))
-    b = numpy.ones(n)
-
-    # deflate out the vector that belongs to the small eigenvalue
-    U = numpy.zeros(n)
-    U[0] = 1.0
-    sol, _ = method(A, b, U=U)
-
-    assert abs(numpy.sum(numpy.abs(sol)) - ref[0]) < tol * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(sol, sol)) - ref[1]) < tol * ref[1]
-    assert abs(numpy.max(numpy.abs(sol)) - ref[2]) < tol * ref[2]
-
-
-def test_deflate_minres():
-    tol = 1.0e-11
-    method = krypy.minres
-    ref = [1004.1873774950692, 1000.0003174918709, 1000.0]
-
-    n = 100
-    A = numpy.diag([1.0e-3] + list(range(2, n + 1)))
-    b = numpy.ones(n)
-
-    # deflate out the vector that belongs to the small eigenvalue
-    U = numpy.zeros(n)
-    U[0] = 1.0
-    sol, _ = method(A, b, U=U)
-
-    assert abs(numpy.sum(numpy.abs(sol)) - ref[0]) < tol * ref[0]
-    assert abs(numpy.sqrt(numpy.dot(sol, sol)) - ref[1]) < tol * ref[1]
-    assert abs(numpy.max(numpy.abs(sol)) - ref[2]) < tol * ref[2]
-
-
-def test_deflate_gmres():
-    tol = 1.0e-11
-    method = krypy.gmres
-    ref = [1004.1873774950692, 1000.0003174918709, 1000.0]
-
     n = 100
     A = numpy.diag([1.0e-3] + list(range(2, n + 1)))
     b = numpy.ones(n)
